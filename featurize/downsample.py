ee040ca4f47d9ba2889daaf4b3003626ff932001
Alvin Wan <hi@alvinwan.com>
2017-06-01 22:43:42 -0700

    fixed downsampling featurization

--- featurize/downsample.py
+++ featurize/downsample.py
@@ -1,7 +1,9 @@
 from .interface import FeaturizeInterface
 from typing import List
 
+import cv2
 import numpy as np
+import os.path
 
 
 class Downsample(FeaturizeInterface):
@@ -15,7 +17,7 @@ class Downsample(FeaturizeInterface):
     def load_model(self):
         pass
 
-    def save_model(self, model):
+    def save_model(self, model, param: str):
         pass
 
     def phi(self, X: np.ndarray, k: float) -> np.array:
@@ -23,6 +25,7 @@ class Downsample(FeaturizeInterface):
         new_w = np.round(self.img_w * k)
         new_h = np.round(self.img_h * k)
         new_d = int(new_w * new_h * self.img_c)
+        assert new_d > 0, 'Too small: Need non-zero dims!'
         newX = np.zeros((X.shape[0], new_d))
         for i in range(X.shape[0]):
             image = X[i].reshape(self.image_shape).astype(np.uint8)
@@ -37,7 +40,9 @@ class Downsample(FeaturizeInterface):
         placeholder = np.zeros((n, 1))
 
         data = np.hstack((X, Y, placeholder))
-        np.savez_compressed(os.path.join(dirname, param, data))
+        encoded_path = os.path.join(self.encoded_dir, param)
+        np.savez_compressed(encoded_path, data)
+        print(' * Saved model to %s' % encoded_path)
 
     def load_encoded(self, params: List[str]):
         pass
