a067a024d80a3ea3fc369762e453dbf70904089e
Alvin Wan <hi@alvinwan.com>
2017-09-03 16:31:05 -0700

    fixed conv sample

--- convs.py
+++ convs.py
@@ -3,7 +3,7 @@
 import numpy as np
 from pictureweb.pictureweb.conv.coates_ng_help import grab_patches
 from pictureweb.pictureweb.conv._conv import conv_compute_output_shape
-from pictureweb.pictureweb.conv._conv import _conv
+from pictureweb.pictureweb.conv._conv import _conv_tf
 from pictureweb.pictureweb.conv.filter_gen import make_empirical_filter_gen_no_mmap
 
 ####################
@@ -15,7 +15,7 @@ batch_feature_size = 512
 num_feature_batches = 8
 data_batch_size = 30
 patch_size = 10
-pool_size = 50
+pool_size = 70
 
 data = np.random.normal(0.0, 10.0, size=(n, 84, 84, 3))
 data = np.transpose(data, axes=(0, 3, 1, 2))
@@ -34,7 +34,7 @@ print(out_shape)
 # Actual conv #
 ###############
 
-out = _conv(
+out = _conv_tf(
     data,
     make_empirical_filter_gen_no_mmap(grab_patches(data, patch_size=patch_size)),
     batch_feature_size,
@@ -43,4 +43,4 @@ out = _conv(
     patch_size=patch_size,
     pool_size=pool_size
 )
-print(out.shape)
+print(out[0].shape)
