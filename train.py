2ac50d35fd832aeb7c8921e3475621b2c5dfab3f
Alvin Wan <hi@alvinwan.com>
2017-09-23 13:28:22 -0700

    action gen for train

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.linalg import solve
 from sklearn.metrics import accuracy_score
 import sys
+import gym
 
 # parse cli
 arguments = sys.argv
@@ -14,13 +15,15 @@ if len(arguments) > 1:
 if len(arguments) > 2:
     env_id = arguments[2]
 
+env = gym.make(env_id)
+
 X = np.load('compute-210x160-%s/X_%s.npy' % (env_id, model_id))
 Y = np.load('compute-210x160-%s/Y_%s.npy' % (env_id, model_id))
 
 X = X.reshape((X.shape[0], X.shape[-1]))
 Y = Y.reshape((Y.shape[0], 1))
 
-Y_oh = np.eye(6)[np.ravel(Y).astype(int)]
+Y_oh = np.eye(env.action_space.n)[np.ravel(Y).astype(int)]
 
 w = solve(X.T.dot(X), X.T.dot(Y_oh))
 np.save('compute-210x160-%s/w_%s.npy' % (env_id, model_id), w)
