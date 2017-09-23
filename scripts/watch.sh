286db1363ca228298a8ff3c20a9988bbca0adf23
Alvin Wan <hi@alvinwan.com>
2017-09-22 22:04:33 -0700

    fixed watch script

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch "ls state-210x160-$1 | wc -l && tmux ls && bash run_distributed_collect.sh"
+watch "ls state-210x160-$1 | wc -l && tmux ls && bash scripts/run_distributed_collect.sh $1"
