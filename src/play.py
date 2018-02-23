fa0a5e7e8ad0dfe6c94d6916bd028994e795e0c4
Alvin Wan <hi@alvinwan.com>
2018-02-23 02:41:41 -0800

    oh shoot this might work

--- src/play.py
+++ src/play.py
@@ -32,24 +32,18 @@ obs_84 = []
 rewards = []
 all_corrects = []
 f = open('../compute-210x160-%s/eval_%s.txt' % (env_id, model_id), 'w')
+print('Starting')
 try:
   for i in range(N):
     last_obs = obs = env.reset()
     reward = episode_reward = 0
     corrects = []
     while True:
-        # obs_84.append(featurize_density(obs).reshape((25, 16, 3)))
-        if len(obs_84) < 4:
-            continue
-        # featurized_obs = np.concatenate(obs_84[-4:], axis=2)
         featurized_obs = featurize_density(obs)
         action = featurized_obs.dot(ls_model).argmax()
         obs, reward, done, info = env.step(action)
-        #state = np.hstack((np.ravel(last_obs), action, reward))
         episode_reward += reward
-        #states.append(state)
         last_obs = obs
-        # obs_84 = obs_84[-4:]
         if done:
             break
     time_id = str(time.time())[-5:]
